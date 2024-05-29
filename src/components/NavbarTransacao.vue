aed7e65c437d3f9b8ca58d1dc0c7fb8092f1846a
victorsantanna <victor.esef@hotmail.com>
2024-05-29 12:41:12 -0300

    feat: cria o componente NavbarTransacao

---
+++ src/components/NavbarTransacao.vue
@@ -0,0 +1,16 @@
+<template>
+     <nav class="navbar-container">
+            <div>
+                <img class="navbar-logo" src="../assets/img/img-home/logo.png" alt="logo stefbank">
+            </div>
+            <div class="navbar-info">
+                <div class="navbar-info-usuario">
+                    <h4>Lucas Silva</h4>
+                    <p>***26320</p>
+                </div>
+                <div>
+                    <button type="button" class="navbar-botao">LOJISTA</button>
+                </div>
+            </div>
+        </nav>
+</template>
