de2b76fa0c807d97124c4b2548aa7e61d2446872
victorsantanna <victor.esef@hotmail.com>
2024-05-31 11:34:08 -0300

    fix: corrigi o erro na busca dos dados da api

--- src/services/contas.js
+++ src/services/contas.js
@@ -3,6 +3,6 @@ import http from './api'
 export default {
     obterContas: async () => {
         const { data } = await http.get(`contas`)
-        return data
+        return data.content;
     },
 }
