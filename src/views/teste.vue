2d42045f117230272710749865c2027f8d29688a
victorsantanna <victor.esef@hotmail.com>
2024-05-31 11:35:09 -0300

    feat: component teste

---
+++ src/views/teste.vue
@@ -0,0 +1,20 @@
+<template>
+    <NavbarTransacaoVue />
+    <MenuLateral />
+    
+</template>
+
+<script>
+import NavbarTransacaoVue from "@/components/NavbarTransacao.vue";
+import MenuLateral from "@/components/MenuLateral.vue";
+export default{
+    components:{
+        NavbarTransacaoVue, MenuLateral,
+    }
+}
+
+</script>
+
+<style>
+
+</style>
