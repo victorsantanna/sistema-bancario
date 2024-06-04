a8b0f6a8dab2df393dc2a65eac4e2497e3a56689
victorsantanna <victor.esef@hotmail.com>
2024-06-04 13:54:37 -0300

    feat: cria função para cadsastrar e obter contas

--- src/services/contas.js
+++ src/services/contas.js
@@ -3,6 +3,14 @@ import http from './api'
 export default {
     obterContas: async () => {
         const { data } = await http.get(`contas`)
-        return data.content;
+        return data;
+    },
+    obterContasPorId: async (parametro) => {
+        const { data } = await http.get(`contas/${parametro}`)
+        return data;
+    },
+    cadastrarConta: async (parametro) => {
+        const { data } = await http.post(`contas`, parametro)
+        return data;
     },
 }
