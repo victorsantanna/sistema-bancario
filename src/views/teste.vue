1d034aedd071e38bb4db5b64b93082ed08c3ce90
victorsantanna <victor.esef@hotmail.com>
2024-06-04 09:01:53 -0300

    feat: importa o component

--- src/views/teste.vue
+++ src/views/teste.vue
@@ -1,15 +1,22 @@
 <template>
-    <NavbarTransacaoVue />
-    <MenuLateral />
+    <div>
+
+        <NavbarTransacaoVue />
+        <MenuLateral />
+        <comprovante-vue />
+    </div>
+    
     
 </template>
 
 <script>
 import NavbarTransacaoVue from "@/components/NavbarTransacao.vue";
 import MenuLateral from "@/components/MenuLateral.vue";
+import ComprovanteVue from '@/components/Comprovante.vue';
+
 export default{
     components:{
-        NavbarTransacaoVue, MenuLateral,
+        NavbarTransacaoVue, MenuLateral, ComprovanteVue,
     }
 }
 
