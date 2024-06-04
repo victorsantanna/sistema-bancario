0300b4af759a8e76dfb101ec0dcfe813e5f922bf
victorsantanna <victor.esef@hotmail.com>
2024-06-04 09:00:36 -0300

    style: estiliza o botão sair

--- src/views/DetalhesView.vue
+++ src/views/DetalhesView.vue
@@ -128,7 +128,7 @@
                 <div class="section-secundaria">
                     <router-link to="/">
                         <div class="botao-sair">
-                            <button> Sair</button>
+                            <button class="btn-sair"> Sair</button>
                             <img src="../assets/img/img-detalhes/frame12.png" alt="">
                         </div>
 
@@ -442,6 +442,23 @@ display: flex;
     display: flex;
     flex-direction: row;
     justify-content: center;
+    margin-right: 50px;
+    margin-bottom: 30px;
+    width: 98px;
+    height: 28px;
+    border: 1px solid #06004F;
+    border-radius: 4px;
+       
+    align-items: center;
+}
+
+.btn-sair{
+    border: none;
+    background-color: #fff;
+    color: #06004F;
+    cursor: pointer;
+    margin-right: 12px;
+    font-weight: bold;
 }
 
 .notificacao{
