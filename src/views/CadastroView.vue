f46a56d55cbbc8e70fd71d6178541da4a44e808f
victorsantanna <victor.esef@hotmail.com>
2024-06-04 13:55:55 -0300

    feat: adiciona o data binding nos inputs

--- src/views/CadastroView.vue
+++ src/views/CadastroView.vue
@@ -11,33 +11,44 @@
                     <div class="form-row">
                         <div class="form-group">
                             <label for="nome">Nome Completo</label>
-                            <input  v-model="usuario.nomeCompleto" type="text" id="nome" name="nome" placeholder="Informe o seu nome completo" required>
+                            <input v-model="usuario.nomeCompleto" type="text" id="nome" name="nome"
+                                placeholder="Informe o seu nome completo" required>
                         </div>
                         <div class="form-group">
                             <label for="cpfcnpj">CPF/CNPJ</label>
-                            <input v-model="usuario.cpfCnpj" type="text" id="cpfcnpj" name="cpfcnpj" placeholder="Informe seu CPF ou CNPJ" required>
+                            <input v-model="usuario.cpfCnpj" type="text" id="cpfcnpj" name="cpfcnpj"
+                                placeholder="Informe seu CPF ou CNPJ" required>
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="form-group">
                             <label for="email">E-mail</label>
-                            <input v-model="usuario.email" type="text" id="email" name="email" placeholder="Informe o endereço de e-mail" required>
+                            <input v-model="usuario.email" type="text" id="email" name="email"
+                                placeholder="Informe o endereço de e-mail" required>
                         </div>
                         <div class="form-group">
                             <label for="celular">Tipo</label>
-                            <input v-model="usuario.tipo" type="text" id="senha3" name="senha3" placeholder="tipo da conta" required>
+                            <input v-model="usuario.tipo" type="text" id="senha3" name="senha3"
+                                placeholder="tipo da conta" required>
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="form-group">
                             <label for="senha">Senha</label>
-                            <input v-model="usuario.senha" type="password" id="senha" name="senha" placeholder="Escolha sua senha de acesso" required>
+                            <input v-model="usuario.senha" type="password" id="senha" name="senha"
+                                placeholder="Escolha sua senha de acesso" required>
                         </div>
                         <div class="form-group">
                             <label for="senha2">Repita a senha</label>
-                            <input type="password" id="senha2" name="senha2" placeholder="Repita sua senha de acesso" >
+                            <input type="password" id="senha2" name="senha2" placeholder="Repita sua senha de acesso">
                         </div>
                     </div>
+                    <div class="">
+                        <label for="conta">Conta</label>
+                        <input v-model="contaUsuario.agencia" type="text" id="conta" name="conta"
+                            placeholder="insira sua conta" max-length="4">
+                    </div>
+
                     <div class="form-group-btn">
                         <button @click="cadastrarUsuario" type="submit">Finalizar cadastro</button>
                         <router-link to="/login">
@@ -49,7 +60,7 @@
         </div>
         <div class="content-img-cadastro">
             <h2 class="texto-img">Prático, rápido e seguro</h2>
-            <img  class="img-cadastro" src="../assets/img/cadastro/imgcadastro.png" alt="logo cadastro">
+            <img class="img-cadastro" src="../assets/img/cadastro/imgcadastro.png" alt="logo cadastro">
         </div>
     </div>
     <footer>
@@ -58,56 +69,67 @@
 </template>
 
 <script>
-import usuarioService from '@/services/usuarios'
-export default{
-    data(){
-        return{
-            usuario:{
-                nomeCompleto:'',
-                cpfCnpj:'',
-                email:'',
-                senha:'',
-                tipo:'',
+import usuarioService from '@/services/usuarios';
+import contaService from '@/services/contas';
+export default {
+    data() {
+        return {
+            usuario: {
+                nomeCompleto: '',
+                cpfCnpj: '',
+                email: '',
+                senha: '',
+                tipo: '',
+            },
+            contaUsuario: {
+                agencia: '',
+                usuario: {
+                    id: ''
+                }
             }
         }
     },
-    methods:{
-     async cadastrarUsuario(){
-       try {
-        await usuarioService.cadastrarUsuario(this.usuario);        
-       } catch (error) {
-        console.error(error.message);
-        if (error.response && error.response.data) {
-         this.error = error.response.data.messageUser || error.response.data.message || 'Erro ao cadastrar usuário';           
-         console.error('Erro detalhado:', error.response.data.messageUser);
-       }
-    }
-  }
-}
+    methods: {
+        async cadastrarUsuario() {
+            try {
+                const response = await usuarioService.cadastrarUsuario(this.usuario);
+                this.contaUsuario.usuario.id = response.id;
+                await contaService.cadastrarConta(this.contaUsuario);
+            } catch (error) {
+                console.error(error.message);
+                if (error.response && error.response.data) {
+                    this.error = error.response.data.messageUser || error.response.data.message || 'Erro ao cadastrar usuário';
+                    console.error('Erro detalhado:', error.response.data.messageUser);
+                }
+            }
+        }
+     }
 }
 </script>
 
 <style scoped>
-
-.container{
+.container {
     display: flex;
     flex-direction: row;
     justify-content: space-around;
-    width:  1240px;
+    width: 1240px;
     height: 520px;
 }
+
 .form-row {
     display: flex;
     justify-content: space-between;
-    margin-bottom: 12px; 
+    margin-bottom: 12px;
 }
-.titulo-cadastro{
+
+.titulo-cadastro {
     display: flex;
     flex-direction: column;
     margin-left: 30px;
     margin-top: 40px;
 }
-.titulo-cadastro-form{
+
+.titulo-cadastro-form {
     margin: 0;
     padding: 0;
     color: #06004F;
@@ -115,7 +137,7 @@ export default{
     font-size: 25px;
 }
 
-.subtitulo-paragrafo{
+.subtitulo-paragrafo {
     margin: 0;
     padding: 0;
     margin-top: 3px;
@@ -124,7 +146,7 @@ export default{
     font-weight: 500;
 }
 
-.content-form-cadastro{
+.content-form-cadastro {
     display: flex;
     flex-direction: row;
     justify-content: center;
@@ -133,80 +155,83 @@ export default{
     width: 500px;
 }
 
-.content-img-cadastro{
+.content-img-cadastro {
     position: relative;
     width: 400px;
 }
 
-nav{
+nav {
     width: 100px;
     height: 50px;
     display: flex
 }
-.img-cadastro{
+
+.img-cadastro {
     width: 420px;
     height: 590px;
 }
-.img-logo-cadastro{
+
+.img-logo-cadastro {
     width: 120px;
     display: flex;
     flex-direction: row;
-    margin:15px;
-}
-.texto-img{
-  position:absolute;
-  z-index: 1;
-  color:#E6E6ED;
-  margin-left:85px;
-  font-size: 38px;
-  margin-top: 50px;
+    margin: 15px;
 }
 
-.footer{
-   width:  1240px;
-   height: 70px;
-   background-color: #06004F;
-   box-shadow: 0 -4px 6px -1px rgba(0, 0, 0, 0.2);
-    
+.texto-img {
+    position: absolute;
+    z-index: 1;
+    color: #E6E6ED;
+    margin-left: 85px;
+    font-size: 38px;
+    margin-top: 50px;
 }
 
-form{
-  display: flex;
-  flex-direction: row;
-  flex-wrap:wrap;
-  justify-content: space-between;
+.footer {
+    width: 1240px;
+    height: 70px;
+    background-color: #06004F;
+    box-shadow: 0 -4px 6px -1px rgba(0, 0, 0, 0.2);
+
 }
 
-.form-group{
+form {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+
+.form-group {
     flex: 0 0 48%;
 }
 
-.form-group input{
+.form-group input {
     border: none;
     border-bottom: 1.3px solid #6B6B6B;
     width: 224px;
     height: 28px;
-    outline: none; 
-    margin-bottom: 20px; 
+    outline: none;
+    margin-bottom: 20px;
     margin-top: 5px;
 }
 
-.form-group input:focus{
-    border-bottom: 2.2px solid #06004F; 
+.form-group input:focus {
+    border-bottom: 2.2px solid #06004F;
 }
 
- label{
+label {
     color: #6B6B6B;
     font-size: 15px;
     font-weight: 700;
 }
 
-.content-cadastro{
+.content-cadastro {
     width: 60%;
     margin-left: 68px;
 }
 
-button[type="submit"]{
+button[type="submit"] {
     background-color: #06004F;
     color: #E6E6ED;
     border: none;
@@ -218,15 +243,14 @@ button[type="submit"]{
     margin-right: 15px;
 }
 
-.btn-naocliente{
+.btn-naocliente {
     width: 240px;
     height: 44px;
-    border: 1.2px solid #06004F; 
+    border: 1.2px solid #06004F;
     color: #06004F;
     border-radius: 4px;
     background-color: #fff;
     font-weight: bold;
     cursor: pointer;
 }
-
 </style>
