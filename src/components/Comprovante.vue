c1b714c92afb68ae2ef8dc9c7717f034ca4a7e64
victorsantanna <victor.esef@hotmail.com>
2024-06-06 11:33:17 -0300

    chore: formatação do código

--- src/components/Comprovante.vue
+++ src/components/Comprovante.vue
@@ -6,7 +6,7 @@
                 <h3> - Comprovante de transferência entre contas</h3>
             </nav>
             <div class="content-comprovante">
-    
+
                 <div class="eminente">
                     <div class="form-row">
                         <label for="">Eminente</label>
@@ -18,7 +18,7 @@
                         <label for="">Horário da transação</label>
                         <span>Victor Lima</span>
                     </div>
-                    <div class="form-row" >
+                    <div class="form-row">
                         <label for="">Agência</label>
                         <span>Victor Lima</span>
                         <label for="">Conta Corrente</label>
@@ -45,15 +45,15 @@
                         <label for="">Conta corrente</label>
                         <span>Victor Lima</span>
                     </div>
-        
+
                 </div>
             </div>
-    
+
             <div class="btn-comprovantes">
                 <button class="btn-baixar">baixar</button>
                 <button>fechar</button>
             </div>
-    
+
         </div>
     </div>
 </template>
@@ -66,90 +66,94 @@ export default {
 
 <style scoped>
 .overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.5); 
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
-.container{
-  background-color: #fff;
-  width: 700px;
-  height: 450px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+.container {
+    background-color: #fff;
+    width: 700px;
+    height: 450px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 }
 
 nav {
-  margin-bottom: 20px;
-  background-color: #06004F;
-  height: 80px;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  
+    margin-bottom: 20px;
+    background-color: #06004F;
+    height: 80px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
 
 }
 
-img{
-width: 90px;
+img {
+    width: 90px;
 }
 
-h3{
+h3 {
     color: #DCDCDC;
     font-size: 15px;
     margin: 5px;
     margin-bottom: 3px;
 }
-.contant-comprovante{
-    display:flex;
+
+.contant-comprovante {
+    display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
 
 }
 
-.eminente, .destinatario{
+.eminente,
+.destinatario {
     width: 100%;
-    
-}
-
-.form-row{
-  display: flex;
-  flex-direction: row;
-  margin-bottom: 5px;
 
 }
 
-label, span {
-  display: block;
-  margin: 10px;
-  margin-bottom: 8px;
-  font-size: 16px;
-  
+.form-row {
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 5px;
+
+}
+
+label,
+span {
+    display: block;
+    margin: 10px;
+    margin-bottom: 8px;
+    font-size: 16px;
+
 }
 
 span {
-  padding: 6px 12px;
-  color: #495057;
-  background-color: #f4f4f4;
-  border: 1px solid #ced4da;
-  border-radius: 4px;
+    padding: 6px 12px;
+    color: #495057;
+    background-color: #f4f4f4;
+    border: 1px solid #ced4da;
+    border-radius: 4px;
 }
+
 .btn-comprovantes {
-  margin-top: 20px;
+    margin-top: 20px;
 }
 
 
-.btn-baixar{
+.btn-baixar {
     border: 1px solid #06004F;
     background-color: #fff;
     color: #06004F;
