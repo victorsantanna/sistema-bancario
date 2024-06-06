aea49a0cbe8dd53460dfe0bedefb025d3e6ad762
victorsantanna <victor.esef@hotmail.com>
2024-06-06 11:34:24 -0300

    chore: formatação do código

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,9 +4,9 @@
         <div class="conteudo-logo">
             <img class="logo" src="../assets/img/img-home/logo.png" alt="logo">
         </div>
-        <RouterLink to="/login" class="no-underline" >
-        <div class="conteudo-entrar">
-            
+        <RouterLink to="/login" class="no-underline">
+            <div class="conteudo-entrar">
+
                 <button class="botao-entrar">Acesse sua conta</button>
                 <img class="imagem-frame" src="../assets/img/img-home/Frame.png" alt="imagem frame">
             </div>
@@ -18,36 +18,36 @@
 <script>
 import { RouterLink } from 'vue-router';
 
-export default{
-    components:{
+export default {
+    components: {
         RouterLink,
     }
 }
 </script>
 
 <style scoped>
-.container{
+.container {
     font-family: 'Montserrat', sans-serif;
     width: 1248px;
     height: 80px;
     background-color: #06004F;
     display: flex;
     flex-direction: row;
-    justify-content:space-between;
+    justify-content: space-between;
     align-items: center;
 }
 
-.conteudo-logo{
+.conteudo-logo {
     margin-left: 50px;
 }
 
-.logo{
+.logo {
     width: 110px;
     height: 22px;
-    
+
 }
 
-.conteudo-entrar{
+.conteudo-entrar {
     margin-top: 5px;
     margin-right: 50px;
     width: 198px;
@@ -60,23 +60,23 @@ export default{
     align-items: center;
 }
 
-.botao-entrar{
+.botao-entrar {
     border: none;
     background-color: #06004F;
     color: #E6E6ED;
     cursor: pointer;
-    
+
 }
 
-.imagem-frame{
+.imagem-frame {
     width: 22px;
     height: 22px;
     cursor: pointer;
     margin-top: 2px;
-    
+
 }
 
-.no-underline{
+.no-underline {
     text-decoration: none;
 }
 </style>
