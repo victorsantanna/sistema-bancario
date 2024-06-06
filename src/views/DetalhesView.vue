875c19a6482e47ba2ce6834108cb00eaa0a0c90d
victorsantanna <victor.esef@hotmail.com>
2024-06-06 11:38:43 -0300

    feat: adiciona funcionalidades da api

--- src/views/DetalhesView.vue
+++ src/views/DetalhesView.vue
@@ -1,79 +1,23 @@
 <template>
+    <NavbarTransacao :nome-usuario="nomeUsuario" :valor-saldo="valorSaldo" :tipo-conta="tipoConta"
+        :cpf-cnpj="cpfCnpj" />
     <div class="container">
-        <nav class="navbar-container">
-            <div>
-                <router-link to="/">
-                    <img class="navbar-logo" src="../assets/img/img-home/logo.png" alt="logo stefbank">
-
-                </router-link>
-            </div>
-            <div class="navbar-info">
-                <div class="navbar-info-usuario">
-                    <h4>Lucas Silva</h4>
-                    <p>***26320</p>
-                </div>
-                <div>
-                    <button type="button" class="navbar-botao">LOJISTA</button>
-                </div>
-            </div>
-        </nav>
         <div class="body-content">
-
-            <aside>
-                <span id="open-btn">
-                    <img src="../assets/img/img-detalhes/expandimenu.png" alt="menu">
-                </span>
-                <div class="aside-info">
-                    <div class="aside-content">
-                        <img class="aside-icon" src="../assets/img/img-detalhes/frame5.png" alt="">
-                        <p>Menu principal</p>
-                    </div>
-                    <router-link to="transacao">
-                    <div class="aside-content">
-                            <img class="aside-icon" src="../assets/img/img-detalhes/frame6.png" alt="">
-                            <p>Realizar transação</p>
-                        </div>
-                    </router-link>
-                    <div class="aside-content">
-                        <img class="aside-icon" src="../assets/img/img-detalhes/frame7.png" alt="">
-                        <p>Histórico</p>
-                    </div>
-                    <div class="aside-content">
-                        <img  class="aside-icon" src="../assets/img/img-detalhes/frame8.png" alt="">
-                        <p>Cartões</p>
-                    </div>
-                    <div class="aside-content">
-                        <img class="aside-icon" src="../assets/img/img-detalhes/frame9.png" alt="">
-                        <p>Investimentos</p>
-                    </div>
-                </div>
-                <div class="aside-config">
-                    <div class="config-info">
-                        <img class="aside-icon" src="../assets/img/img-detalhes/frame10.png" alt="">
-                        <p>Configurações</p>
-                    </div>
-                    <div class="config-usuario">
-                        <div>
-                            <img class="config-usuario-img" src="../assets/img/img-detalhes/Ellipse1.png" alt="">
-                        </div>
-                        <div class="config-usuario-info">
-                            <p>Lucas Silva</p>
-                            <button>LOJISTA</button>
-                        </div>
-                    </div>
-                </div>
-            </aside>
-    
+            <MenuLateral />
             <section class="content-section">
                 <div class="content-info-section">
                     <div class="content-info-usuario">
-                        <h3>Bem-vindo, Lucas Silva</h3>
+                        <h3>Bem-vindo(a), {{ this.nomeUsuario }}</h3>
                         <div class="content-saldo-usuario">
-                           <p>Saldo em conta</p>
-                           <img src="../assets/img/img-detalhes/frame2.png" alt="olho">
+                            <p>Saldo em conta</p>
+                            <img v-if="olhoFechado" @click="toggleOlho" class="img-olho"
+                                src="../assets/img/img-detalhes/frame2.png" alt="olho" />
+                            <img v-else @click="toggleOlho" class="img-olho"
+                                src="../assets/img/img-detalhes/olhofechado.png" alt="olhofechado" />
                         </div>
                         <strong>
-                            <p>R$:1400,45</p>
+                            <p v-if="olhoFechado">{{ formatarMoeda(valorSaldo) }}</p>
+                            <p v-else class="oculto">*********</p>
                         </strong>
                     </div>
                     <div class="content-transacoes-info">
@@ -91,7 +35,7 @@
                                         <img src="../assets/img/img-detalhes/frame3.png" alt="">
                                     </div>
                                 </div>
-                                <div  class="content-transacoes-usuario">
+                                <div class="content-transacoes-usuario">
                                     <div class="content-transacoes-info">
                                         <h4>Transferência realizada</h4>
                                         <p>R$: 140,00</p>
@@ -101,7 +45,7 @@
                                         <img src="../assets/img/img-detalhes/frame4.png" alt="">
                                     </div>
                                 </div>
-                                <div  class="content-transacoes-usuario">
+                                <div class="content-transacoes-usuario">
                                     <div class="content-transacoes-info">
                                         <h4>Transferência realizada</h4>
                                         <p>R$: 140,00</p>
@@ -111,7 +55,7 @@
                                         <img src="../assets/img/img-detalhes/frame4.png" alt="">
                                     </div>
                                 </div>
-                                <div  class="content-transacoes-usuario">
+                                <div class="content-transacoes-usuario">
                                     <div class="content-transacoes-info">
                                         <h4>Transferência realizada</h4>
                                         <p>R$: 140,00</p>
@@ -122,7 +66,7 @@
                                     </div>
                                 </div>
                             </div>
-                         </div>
+                        </div>
                     </div>
                 </div>
                 <div class="section-secundaria">
@@ -134,244 +78,165 @@
 
                     </router-link>
                     <div>
-                        <img class="section-banner" src="../assets/img/img-detalhes/banner1.png" alt="">
+                        <img class="section-banner" :src="imagemAtual" alt="">
                     </div>
                 </div>
             </section>
+
         </div>
 
     </div>
 
-    
+
 </template>
 
 <script>
 import { RouterLink } from 'vue-router';
-import contasService from '@/services/contas.js';
-import transacoesService from '@/services/transacoes.js';
+import contasService from '@/services/contasService.js';
+import transacoesService from '@/services/transacoesService.js';
+import NavbarTransacao from '@/components/NavbarTransacao.vue';
+import MenuLateral from '@/components/MenuLateral.vue';
 
-export default{
-  created(){
-    this.getContas();
-    this.getTransacoes();
-  },
-  methods:{
-     async getContas(){
-       try {
-        const response = await contasService.obterContas();
-        console.log(response);
-        
-        return response
-       } catch (error) {
-        console.error(error);
-       }
+export default {
+    components: {
+        NavbarTransacao, MenuLateral,
     },
-    async getTransacoes(){
-         try {
-          const response = await transacoesService.obterTransacoes();
-          console.log(response);
-          
-          return response
-         } catch (error) {
-          console.error(error);
-         }
-      },
-    async getContaPorId(){
-         try {
-          const response = await contasService.obterContasPorId();
-          console.log(response);
-          
-          return response
-         } catch (error) {
-          console.error(error);
-         }
-      },
+    data() {
+        return {
+            olhoFechado: true,
+            indiceAtual: 0,
+            imagens: [
+                require('@/assets/img/img-detalhes/banner1.png'),
+                require('@/assets/img/img-detalhes/banner2.png'),
+                require('@/assets/img/img-detalhes/banner3.png')
+            ],
+            nomeUsuario: '',
+            valorSaldo: '',
+            tipoConta: '',
+            cpfCnpj: '',
 
-  },
+        }
+    },
+    computed: {
+        imagemAtual() {
+            return this.imagens[this.indiceAtual];
+        },
+    },
+    mounted() {
+        setInterval(() => {
+            this.indiceAtual = (this.indiceAtual + 1) % this.imagens.length;
+        }, 5000);
+    },
+    created() {
+        this.getContas();
+        this.getTransacoes();
+        this.getContaPorId();
+    },
+    methods: {
+        toggleOlho() {
+            this.olhoFechado = !this.olhoFechado;
+        },
+        async getContas() {
+            try {
+                const response = await contasService.obterContas();
+                console.log(response);
+
+                return response
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        async getTransacoes() {
+            try {
+                const response = await transacoesService.obterTransacoes();
+                console.log(response);
+
+                return response
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        async getContaPorId() {
+            try {
+                const idUsuario = sessionStorage.getItem('idUsuario');
+                console.log('Id do usuário ' + idUsuario)
+                const response = await contasService.obterContasPorId(sessionStorage.getItem('idUsuario'));
+                console.log(response);
+                this.nomeUsuario = response.usuario.nomeCompleto;
+                this.valorSaldo = response.saldo;
+                this.tipoConta = response.usuario.tipo;
+                this.cpfCnpj = response.usuario.cpfCnpj;
+                return response
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        formatarMoeda(valor) {
+            return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(valor);
+        },
+        formatarCpfCnpj(cpfCnpj) {
+            if (cpfCnpj && cpfCnpj.length === 11) {
+                return cpfCnpj.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})$/, "***.$2.$3-**");
+            } else if (cpfCnpj && cpfCnpj.length === 11) {
+                return cpfCnpj.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/, "**.$2.$3/****");
+            } else {
+                return cpfCnpj;
+            }
+        }
+
+    },
+
 }
 
 </script>
 
 <style scoped>
+.container {
+    width: 1240px;
+    height: 585px;
 
-.container{
-  width: 1240px;
-  height: 585px;
- 
 }
-.body-content{
+
+.body-content {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
 }
 
-nav{
-    font-family: 'Montserrat', sans-serif;
-    width: 1248px;
-    height: 80px;
-    background-color: #06004F;
+.content-section {
+    display: flex;
+}
+
+.content-section {
     display: flex;
     flex-direction: row;
-    justify-content:space-between;
-    align-items: center;
+    justify-content: space-around;
 }
 
-.navbar-logo{
-    margin-left: 60px;
-    width: 120px;
-}
-
-.navbar-info{
-    color: #E6E6ED;
-    display: flex;
-    flex-direction: row;
-    justify-content:space-between;
-    align-items: center;
-    margin: 50px;
-}
-.navbar-info-usuario{
-    margin: 20px;
-    
-}
-
-.navbar-info-usuario h4{
-    font-size: 16px;
-}
-
-.navbar-info-usuario p{
-    text-indent: 33px;
-    font-size: 14px;
-}
-
-.navbar-info-usuario h4,
-.navbar-info-usuario p {
-  margin: 0;
-  padding: 0;
-}
-
-.navbar-botao{
-    background-color: #1D1393;
-    border: .5px solid #3b348b;
-    color: #E6E6ED;
-    padding: 7px 20px;
-    border-radius: 4px;
-    cursor: pointer;
-}
-
-aside{
-    width: 200px;
-    background-color: #05003F;
-    padding: 20px;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-    height: 475px;
-    
-}
-#open-btn{
-    margin-left: 202px;
-}
-
-.aside-content{
-    margin-bottom: 30px;
-    cursor: pointer;
-}
-.aside-icon{
-    width: 18px;
-    height: 18px;
-    margin-right: 10px;
-}
-
-.aside-content{
-    display: flex;
-    align-items: center;
-    margin-bottom: 15px;
-}
-/* Estilos para textos do menu */
-aside p {
-    margin: 0;
-    font-size: 16px;
-    color: #E6E6ED;
-}
-
-.aside-config{
-    margin-top: 130px;
-    padding-top: 20px;
-}
-
-.config-usuario{
-    display: flex;
-    flex-direction: row;
-    margin-top: 30px;
-    justify-content: center;
-    
-}
-.config-usuario-img{
-    width: 50px;
-}
-
-.config-usuario-info{
-    margin-left: 20px;
-    font-size: 20px;
-    text-indent: -6px;
-}
-
-        aside > div:last-child > div {
-            display: flex;
-            align-items: center;
-        }
-
-        aside > div:last-child img {
-           
-            border-radius: 50%;
-        }
-
-        aside > div:last-child p {
-            margin-left: 10px;
-            font-weight: bold;
-        }
-
-        aside > div:last-child button {
-            background-color: #1D1393;
-    border: .5px solid #3b348b;
-    color: #E6E6ED;
-    padding: 4px 18px;
-    border-radius: 4px;
-    cursor: pointer;
-        }
-
-        aside > div:last-child button:hover {
-            background-color: #0056b3;
-        }
-
-.content-section{
-display: flex;
-}
-
-.content-section{
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;   
-}
-
-.content-info-section{
+.content-info-section {
     margin-right: 250px;
 }
-.content-saldo-usuario{
+
+.content-saldo-usuario {
     display: flex;
     flex-direction: row;
 }
-.content-saldo-usuario img{
+
+.content-saldo-usuario img {
     margin-left: 10px;
 }
 
-.content-info-usuario{
+.content-info-usuario {
     display: flex;
     flex-direction: column;
-    margin:0;
+    margin: 0;
     padding: 0;
     margin-bottom: 40px;
-    
+
 }
-.content-info-usuario p, h3{
+
+.content-info-usuario p,
+h3 {
     margin: 0;
     padding: 0;
 }
@@ -380,7 +245,7 @@ display: flex;
     margin-top: 40px;
 }
 
-.content-transacoes-usuario{
+.content-transacoes-usuario {
     display: flex;
     flex-direction: row;
     justify-content: center;
@@ -390,66 +255,78 @@ display: flex;
 
 }
 
-.content-transacoes-usuario img{
+.content-transacoes-usuario img {
     margin-left: 90px;
     padding: 15px;
 }
 
-.content-transacoes-info{
+.content-transacoes-info {
     display: flex;
     flex-direction: column;
-    
-    
+
+
 }
-.content-transacoes-info p, h4{
+
+.content-transacoes-info p,
+h4 {
     margin: 0;
     padding: 0;
 }
-.content-transacoes-info h4{
+
+.content-transacoes-info h4 {
     margin-top: 30px;
 }
 
-.section-banner{
+.section-banner {
     width: 190px;
     margin-right: 60px;
 }
 
 .scroll-box {
     width: 100%;
-    height: 300px; /* Defina a altura do box para ativar a rolagem */
+    height: 300px;
+    /* Defina a altura do box para ativar a rolagem */
     padding: 10px;
     border: 1px solid #000;
     border: none;
     background-color: #fff;
-    overflow-y: scroll; /* Ativa a rolagem vertical */
-    scrollbar-width: thin; 
-    
+    overflow-y: scroll;
+    /* Ativa a rolagem vertical */
+    scrollbar-width: thin;
+
 }
 
 .scroll-box::-webkit-scrollbar {
-    width: 12px; /* Largura da barra de rolagem */
-    
+    width: 12px;
+    /* Largura da barra de rolagem */
+
 }
 
 .scroll-box::-webkit-scrollbar-track {
-    background: #000000; /* Cor do trilho da barra de rolagem */
-    border-radius: 10px; /* Remove as bordas */
-}
-.scroll-box::-webkit-scrollbar-thumb {
-    background: #888; /* Cor do polegar da barra de rolagem */
-    border-radius: 10px; /* Remove as bordas */
-    border: none; /* Remove qualquer borda */
-    
+    background: #000000;
+    /* Cor do trilho da barra de rolagem */
+    border-radius: 10px;
+    /* Remove as bordas */
 }
 
-.section-secundaria{
+.scroll-box::-webkit-scrollbar-thumb {
+    background: #888;
+    /* Cor do polegar da barra de rolagem */
+    border-radius: 10px;
+    /* Remove as bordas */
+    border: none;
+    /* Remove qualquer borda */
+
+}
+
+.section-secundaria {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: flex-end;
 }
 
-.botao-sair{
+.botao-sair {
     display: flex;
     flex-direction: row;
     justify-content: center;
@@ -459,11 +336,11 @@ display: flex;
     height: 28px;
     border: 1px solid #06004F;
     border-radius: 4px;
-       
+
     align-items: center;
 }
 
-.btn-sair{
+.btn-sair {
     border: none;
     background-color: #fff;
     color: #06004F;
@@ -472,9 +349,26 @@ display: flex;
     font-weight: bold;
 }
 
-.notificacao{
+.notificacao {
     display: flex;
     flex-direction: row;
     justify-content: center;
+}
+
+.img-olho {
+    cursor: pointer;
+}
+
+.oculto {
+    background-color: #D9D9D9;
+    /* Cor de fundo cinza claro */
+    padding: 10px 20px;
+    width: 100px;
+    /* Espaçamento interno */
+    border-radius: 5px;
+    /* Borda arredondada */
+    display: inline-block;
+    /* Para que o elemento se comporte como um bloco */
+    color: #D9D9D9;
 }
 </style>
