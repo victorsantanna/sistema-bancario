ab2d77ff98f09379491d3f4fc9f5c2ad214dec2f
victorsantanna <victor.esef@hotmail.com>
2024-06-06 11:34:45 -0300

    feat: adiciona props

--- src/components/NavbarTransacao.vue
+++ src/components/NavbarTransacao.vue
@@ -1,73 +1,108 @@
 <template>
-     <nav class="navbar-container">
-            <router-link to="/" >
-                <div>
-                    <img class="navbar-logo" src="../assets/img/img-home/logo.png" alt="logo stefbank">
-                </div>
-
-            </router-link>
-            <div class="navbar-info">
-                <div class="navbar-info-usuario">
-                    <h4>Lucas Silva</h4>
-                    <p>***26320</p>
-                </div>
-                <div>
-                    <button type="button" class="navbar-botao">LOJISTA</button>
-                </div>
+    <nav class="navbar-container">
+        <router-link to="/">
+            <div>
+                <img class="navbar-logo" src="../assets/img/img-home/logo.png" alt="logo stefbank">
             </div>
-        </nav>
+
+        </router-link>
+        <div class="navbar-info">
+            <div class="navbar-info-usuario">
+                <h4>{{ this.nomeUsuario }}</h4>
+                <p>{{ formatarCpfCnpj(cpfCnpj) }}</p>
+            </div>
+            <div>
+                <button type="button" class="navbar-botao">{{ this.tipoConta }}</button>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script>
+export default {
+    props: {
+
+        nomeUsuario: {
+            type: String,
+            required: true
+        },
+        valorSaldo: {
+            type: Number,
+            required: true
+        },
+        tipoConta: {
+            type: String,
+            required: true
+        },
+        cpfCnpj: {
+            type: String,
+            required: true
+        }
+    },
+    methods:{
+        formatarCpfCnpj(cpfCnpj) {
+            if (cpfCnpj && cpfCnpj.length === 11) {
+                return cpfCnpj.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})$/, "***.$2.$3-**");
+            } else if (cpfCnpj && cpfCnpj.length === 11) {
+                return cpfCnpj.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/, "**.$2.$3/****");
+            } else {
+                return cpfCnpj;
+            }
+        }
+    }
+}
+
+
 
 </script>
 
 <style scoped>
-    nav{
+nav {
     font-family: 'Montserrat', sans-serif;
     width: 1248px;
     height: 80px;
     background-color: #06004F;
     display: flex;
     flex-direction: row;
-    justify-content:space-between;
+    justify-content: space-between;
     align-items: center;
 }
 
-.navbar-logo{
+.navbar-logo {
     margin-left: 60px;
     width: 120px;
 }
 
-.navbar-info{
+.navbar-info {
     color: #E6E6ED;
     display: flex;
     flex-direction: row;
-    justify-content:space-between;
+    justify-content: space-between;
     align-items: center;
     margin: 50px;
 }
-.navbar-info-usuario{
+
+.navbar-info-usuario {
     margin: 20px;
-    
+
 }
 
-.navbar-info-usuario h4{
+.navbar-info-usuario h4 {
     font-size: 16px;
 }
 
-.navbar-info-usuario p{
+.navbar-info-usuario p {
     text-indent: 33px;
     font-size: 14px;
 }
 
 .navbar-info-usuario h4,
 .navbar-info-usuario p {
-  margin: 0;
-  padding: 0;
+    margin: 0;
+    padding: 0;
 }
 
-.navbar-botao{
+.navbar-botao {
     background-color: #1D1393;
     border: .5px solid #3b348b;
     color: #E6E6ED;
