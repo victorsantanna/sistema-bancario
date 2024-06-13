4bf2641741c5eecb3883ecc7456a94f4417255f9
victorsantanna <victor.esef@hotmail.com>
2024-06-13 19:23:57 -0300

    feat: adiciona data para receber as informações da api

--- src/views/HistoricoView.vue
+++ src/views/HistoricoView.vue
@@ -1,85 +1,107 @@
-<template>
-    <NavbarTransacao />
-    <div class="container">
-        <MenuLateral />
-        <div class="conteudo-principal">
-            <div class="btn-sair">
-                <button>Sair</button>
-                <img src="../assets/img/img-detalhes/frame12.png" alt="">
-            </div>
-            <div class="historico">
-                <h3>Historico de transação</h3>
-                <div class="historico-data">
-                    <p>Data de inicio</p>
-                    <input type="date">
-                    <p>Data de fim</p>
-                    <input type="date">
+    <template>
+        <NavbarTransacao :nome-usuario="nomeUsuario" :valor-saldo="valorSaldo" :tipo-conta="tipoConta"
+            :cpf-cnpj="cpfCnpj" />
+        <div class="container">
+            <MenuLateral :nome-usuario="nomeUsuario" :tipo-conta="tipoConta" :cpf-cnpj="cpfCnpj" />
+            <div class="conteudo-principal">
+                <BotaoSair class="btn-sair" />
+                <div class="historico">
+                    <h3>Historico de transação</h3>
+                    <div class="historico-data">
+                        <div class="historico-data-info">
+                            <p>Data de inicio</p>
+                            <input class="custom-date-input" type="date">
+                        </div>
+                        <div class="historico-data-info">
+                            <p>Data de fim</p>
+                            <input class="custom-date-input" type="date">
+                        </div>
+                        <div class="btn-buscar-historico">
+                            <img src="../assets/img/historico/lupa.png" alt="" class="btn-lupa">
+                            <button class="btn-buscar">Buscar</button>
+                        </div>
 
+                    </div>
                 </div>
-            </div>
-            <div class="conteudo-historico">
-                <h3>mês</h3>
-                <p>data</p>
-                <input v-model="buscarPorId" type="text">
-                <button @click="transacaoPorid">buscar</button>
-                <div class="conteudo-historico-info">
-                    <div>
-                        <img src="../assets/img/historico/deposito.png" alt="">
-                    </div>
-                    <div>
-                        <h3>transferencia recebida</h3>
-                        <p>R$:150,00</p>
-                        <p>Lucas Silva</p>
-                    </div>
-                    <div>
-                        <h3>CPF/CNPJ</h3>
-                        <p>***.123.774-**</p>
-                    </div>
-                    <div>
-                        <h3>Agência</h3>
-                        <p>1</p>
-                    </div>
-                    <div>
-                        <h3>Número da conta</h3>
-                        <p>88996633-1</p>
-                    </div>
-                    <div>
-                        <h3>Horário</h3>
-                        <p>17h23</p>
-                    </div>
-                    <div>
-                        <img src="../assets/img/historico/comprovante.png" alt="">
+                <div class="conteudo-historico">
+
+                    <div class="conteudo-historico-info">
+                        <div class="historico-group">
+                            <img src="../assets/img/historico/deposito.png" alt="">
+                        </div>
+                        <div class="historico-group">
+                            <h3>Transferência</h3>
+                            <p>R$:150,00</p>
+                            <p>Lucas Silva</p>
+                        </div>
+                        <div class="historico-group">
+                            <h3>CPF/CNPJ</h3>
+                            <p>***.123.774-**</p>
+                        </div>
+                        <div class="historico-group">
+                            <h3>Agência</h3>
+                            <p>1</p>
+                        </div>
+                        <div class="historico-group">
+                            <h3>Número da conta</h3>
+                            <p>889963-1</p>
+                        </div>
+                        <div class="historico-group">
+                            <h3>Horário</h3>
+                            <p>17h23</p>
+                        </div>
+                        <div class="historico-group">
+                            <img class="historico-boleto-img" src="../assets/img/historico/comprovante.png"
+                                alt="boleto">
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-</template>
+    </template>
 
 <script>
 import transacoesService from '@/services/transacoesService.js';
 import NavbarTransacao from '@/components/NavbarTransacao.vue';
 import MenuLateral from '@/components/MenuLateral.vue';
+import BotaoSair from '@/components/BotaoSair.vue';
 export default {
     components: {
-        NavbarTransacao, MenuLateral,
+        NavbarTransacao, MenuLateral, BotaoSair,
     },
-    data(){
-        return{
+    data() {
+        return {
             buscarPorId: '',
+            nomeUsuario: '',
+            valorSaldo: 0,
+            tipoConta: '',
+            cpfCnpj: '',
+            contaOrigem: {
+                agencia: '',
+                conta: '',
+                id: '',
+                saldo: '',
+            }
+
         }
     },
-    methods:{
-        async transacaoPorid(){
+    methods: {
+        async transacaoPorid() {
             try {
                 const response = await transacoesService.obterTransacaoPorId(this.buscarPorId);
                 console.log(response);
                 return response
             } catch (error) {
-                
+
             }
         }
+    },
+    created() {
+        this.nomeUsuario = sessionStorage.getItem('nomeUsuario') || '';
+        this.valorSaldo = Number(sessionStorage.getItem('valorSaldo')) || 0;
+        this.tipoConta = sessionStorage.getItem('tipoConta') || '';
+        this.cpfCnpj = sessionStorage.getItem('cpfCnpj') || '';
     }
 }
 </script>
@@ -88,39 +110,135 @@ export default {
 .container {
     display: flex;
     font-family: 'Montserrat', sans-serif;
+    width: 1240px;
+    height: 585px;
 }
 
 .conteudo-principal {
     display: flex;
     flex-direction: column;
     justify-content: center;
-    margin-left: 100px;
-    margin-top: 0;
-    width: 100%;
-    height: 500px;
+    margin: 11px;
+    margin-left: 80px;
+    width: 67%;
+    height: 290px;
+    
 }
 
 .btn-sair {
     display: flex;
     justify-content: flex-end;
+    
+
+
 }
 
 .historico {
     display: flex;
     flex-direction: column;
+    
+    margin-bottom: 20px;
 }
 
 .historico-data {
     display: flex;
+    justify-content: flex-start;
+    align-items: center;
+
+}
+
+.historico-data-info {
+    margin: 5px;
+}
+
+.historico-data-info p {
+    font-size: 13px;
 }
 
 .conteudo-historico {
     display: flex;
     flex-direction: column;
+    justify-content: flex-start;
 }
 
 .conteudo-historico-info {
     display: flex;
+    justify-content: center;
+    align-items: flex-start;
+   
+
 
 }
+
+.historico-group {
+    margin: 0;
+    padding: 0;
+
+    margin-right: 40px;
+    display: inline-block;
+
+
+}
+
+.historico-group h3 {
+    font-size: 15px;
+    margin: 0;
+    padding: 0;
+    white-space: nowrap;
+
+}
+
+.historico-group p {
+    font-size: 15px;
+    white-space: nowrap;
+    margin: 0;
+    padding: auto;
+
+
+}
+
+.historico-group img {
+    margin-top: 13px;
+}
+
+.btn-buscar-historico {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 90px;
+    height: 40px;
+    border: 1.2px solid #06004F;
+    color: #DCDCDC;
+    border-radius: 4px;
+    background-color: #06004F;
+    font-weight: bold;
+    cursor: pointer;
+    margin-top: 40px;
+}
+
+.btn-buscar {
+    color: #DCDCDC;
+    background-color: #06004F;
+    cursor: pointer;
+    border: none;
+}
+
+.btn-lupa {
+    width: 20px;
+    height: 20px;
+}
+
+.custom-date-input {
+    width: 150px;
+    height: 40px;
+    font-size: 17px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    margin: 0;
+}
+
+.historico-boleto-img {
+    cursor: pointer;
+}
 </style>
+
