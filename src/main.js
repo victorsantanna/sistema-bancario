d1ec1c5b830756723ffab8102c14ac50ef1c658f
victorsantanna <victor.esef@hotmail.com>
2024-06-21 06:50:52 -0300

    feat: importa o v-money globalmente

--- src/main.js
+++ src/main.js
@@ -4,13 +4,13 @@ import axios from 'axios';
 import router from './router';
 import { mask } from 'vue-the-mask';
 import VueElementLoading from 'vue-element-loading';
-
-
+import money from 'v-money';
 
 
 
 createApp(App)
     .component('VueElementLoading', VueElementLoading)
+    .use(money)
     .directive('mask', mask)
     .use(router)
     .mount('#app')
